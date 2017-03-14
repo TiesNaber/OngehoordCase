4b42ef6c62308c1d80aed4c45316867673b77cb3
TiesNaber <tiesnaber@live.nl>
2017-03-14 14:41:52 +0100

    Music Preview

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour {
+
+    public float lookingLength;
+    public float originPos;
+    public float hoverDis;
+    bool seeing = false;
+
+    
+   
+    public GameObject selectButton;
+
+
+    AudioSource audio;
+    [SerializeField]
+    AudioSource[] songs = new AudioSource[2];
+   
+
+
+
+
+    // Use this for initialization
+   void Start ()
+    {
+        
+	}
+
+    
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        RaycastHit lookAt;
+        Ray rayDir = new Ray((transform.position), transform.forward);
+
+        Debug.DrawRay(transform.position, transform.forward * lookingLength, Color.red);
+        if(Physics.Raycast(rayDir, out lookAt, lookingLength))
+        {                        
+                if (lookAt.collider.gameObject.tag == "Button" && !lookAt.collider.GetComponent<AudioSource>().isPlaying)
+                {
+
+                audio = lookAt.collider.GetComponent<AudioSource>();
+                audio.time = 30;
+                audio.Play();
+                Debug.Log("Looking");
+                                
+
+                }
+            audio.enabled = true;
+          
+
+        }
+
+        else
+        {
+            if (audio != null)
+            {
+                audio.enabled = false;
+                audio = null;
+
+            }
+
+           
+            
+           
+            
+        }
+
+        
+    }
+
+    
+
+ 
+}
---
+++ Assets/Scripts/mesh.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mesh : MonoBehaviour {
+
+    public GameObject Prefab;
+
+    public float tranSpeed = 2f;
+    Vector3 temp;
+    // Use this for initialization
+    void Start () {
+       
+        
+        GenerateTree(4, transform);
+	}
+
+    void Update()
+    {
+        temp = transform.position;
+        temp.x += tranSpeed * Time.deltaTime;
+        transform.localPosition = temp;
+    }
+
+
+    
+    
+    void GenerateTree(int depth, Transform parent)
+    {
+        
+        
+        if(depth <= 0)
+        {
+            return;
+        }
+
+        GameObject cube = (GameObject)Instantiate(Prefab, parent);
+        cube.transform.localPosition = new Vector3(1.0f, 0.0f, 0.0f);
+        
+        
+        GenerateTree( depth -1, cube.transform);
+
+
+
+       
+    }
+	
+	
+}
