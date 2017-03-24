d1df3d6b253cd885622ed9df7aca5502735c18a6
TiesNaber <tiesnaber@live.nl>
2017-03-24 11:01:19 +0100

    Nothing Changed

---
+++ Assets/Scripts/PlaneDropDown.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneDropDown : MonoBehaviour {
+
+    public Transform target;
+    public float speed;
+    float timeBetween = 1F;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        transform.rotation = Quaternion.Lerp(transform.rotation, target.rotation, step * Time.deltaTime );
+
+        
+		
+	}
+
+    IEnumerator TransitionToEar()
+    {
+
+        yield return new WaitForSeconds(timeBetween);
+    }
+
+
+       
+}
